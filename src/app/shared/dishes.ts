774d7c8fb3607ecc6360737d98befabc007b4774
Goutham <gouthemannem@gmail.com>
2018-09-03 09:54:46 -0700

    Angular Services setup

---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,165 @@
+import { Dish } from './dish';
+
+export const DISHES: Dish[] = [
+    {
+        name: 'Family Portrait',
+        image: '/assets/images/uthappizza.jpg',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        // tslint:disable-next-line:max-line-length
+        description: "Family is one of nature's masterpieces. It means putting your arms around each other and being there.",
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'John Lemon',
+                date: '2012-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Paul McVites',
+                date: '2014-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Michael Jaikishan',
+                date: '2015-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ringo Starry',
+                date: '2013-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: '25 Cent',
+                date: '2011-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        name: 'Friends',
+        image: '/assets/images/zucchipakoda.jpg',
+        category: 'appetizer',
+        label: '',
+        price: '1.99',
+        description: 'Friends are the family we get to choose ourselves. There are friends, there is family and there are friends that become family',
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'John Lemon',
+                date: '2012-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Paul McVites',
+                date: '2014-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Michael Jaikishan',
+                date: '2015-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ringo Starry',
+                date: '2013-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: '25 Cent',
+                date: '2011-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        name: 'Couple & Valentine',
+        image: '/assets/images/vadonut.jpg',
+        category: 'appetizer',
+        label: 'New',
+        price: '1.99',
+        description: "Love is when the other person's happiness is more important than your own.Being deeply loved by someone gives you the strength, while loving someone deeply gives you courage",
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'John Lemon',
+                date: '2012-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Paul McVites',
+                date: '2014-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Michael Jaikishan',
+                date: '2015-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ringo Starry',
+                date: '2013-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: '25 Cent',
+                date: '2011-12-02T17:57:28.556094Z'
+            }
+        ]
+    },
+    {
+        name: 'Famous Personalities & Icons',
+        image: '/assets/images/elaicheesecake.jpg',
+        category: 'dessert',
+        label: '',
+        price: '2.99',
+        description: "A day without a laughter is a day wasted. Two things define your personality, the way you manage things when you have nothing, and the way you behave when you have everything.",
+        comments: [
+            {
+                rating: 5,
+                comment: 'Imagine all the eatables, living in conFusion!',
+                author: 'John Lemon',
+                date: '2012-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
+                author: 'Paul McVites',
+                date: '2014-09-05T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                comment: 'Eat it, just eat it!',
+                author: 'Michael Jaikishan',
+                date: '2015-02-13T17:57:28.556094Z'
+            },
+            {
+                rating: 4,
+                comment: 'Ultimate, Reaching for the stars!',
+                author: 'Ringo Starry',
+                date: '2013-12-02T17:57:28.556094Z'
+            },
+            {
+                rating: 2,
+                comment: 'It\'s your birthday, we\'re gonna party!',
+                author: '25 Cent',
+                date: '2011-12-02T17:57:28.556094Z'
+            }
+        ]
+    }
+];
